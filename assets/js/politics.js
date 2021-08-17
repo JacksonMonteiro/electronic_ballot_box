a72c12c63cac97698ca3a4356023cf36d7544760
jacksonmonteiro <infor.jackson324@gmail.com>
2021-08-17 20:57:34 -0300

    some features

--- assets/js/politics.js
+++ assets/js/politics.js
@@ -7,26 +7,22 @@ const politicsJSON = [
 		[
 			// First Politician
 			{
-				number: 49222,
+				number: '49222',
 				name: 'Politics 1',
 				party: 'A',
 				photo:
 				[
-					{
-						url: '../images/biden.jpg'
-					}
+					{ url: '../images/biden.jpg', legend: 'Biden', class: 'big' }
 				]
 			},
 			// Second Politician
 			{
-				number: 88333,
+				number: '88333',
 				name: 'Politics 2',
 				party: 'B',
 				photo:
 				[
-					{
-						url: '../images/obama.jpg'
-					}
+					{ url: '../images/obama.jpg', legend: 'Obama', class: 'big' }
 				]
 			}
 		]
@@ -40,26 +36,26 @@ const politicsJSON = [
 		[
 			// First Politician
 			{
-				number: 12,
+				number: '12',
 				name: 'Politics 3',
 				party: 'A',
 				vice: 'Obama',
 				photo:
 				[
-					{ url: '../images/biden.jpg' },
-					{ url: '../images/obama.jpg', small: true }
+					{ url: '../images/biden.jpg', legend: 'Biden', big: true },
+					{ url: '../images/obama.jpg', legend: 'Obama', small: true }
 				]
 			},
 			// Second Politician
 			{
-				number: 24,
+				number: '24',
 				name: 'Politics 4',
 				party: 'B',
 				vice: 'Biden',
 				photo:
 				[
-					{ url: '../images/obama.jpg' },
-					{ url: '../images/biden.jpg', small: true }
+					{ url: '../images/obama.jpg', legend: 'Obama', big: true },
+					{ url: '../images/biden.jpg', legend: 'Biden', small: true }
 				]
 			}
 		]
--- assets/js/script.js
+++ assets/js/script.js
@@ -3,14 +3,107 @@ const qs = e => document.querySelector( e );
 const qsa = e => document.querySelectorAll( e );
 
 // Variables
+	// Left
+	let vote = qs( '.content-left-1 span' );
+	let position = qs( '.content-left-2 span' );
+	let numbers = qs( '.content-left-3' );
+	let description = qs( '.content-left-4 ' );
+	let warn = qs( '.info' );
 
-// Left
-let vote = qs( '.content-left-1 span' );
-let position = qs( '.content-left-2 span' );
-let numbers = qs( '.content-left-3' );
-let description = qs( '.content-left-4 span' );
-let warn = qs( '.info' );
 
+	// Right
+	let contentRight = qs( '.content-right' );
 
-// Right
-let contentRight = qs( '.content-right' );
+	// Control System
+	let actualStage = 0;
+	let typedNumber = '';
+
+	function beginStage() {
+		let stage = politicsJSON[actualStage];
+		let htmlNumber = '';
+
+		for (let i = 0; i < stage.numbers; i++) {
+			if (i === 0) {
+				htmlNumber += '<span class="number blink"></span>'
+			} else {
+				htmlNumber += '<span class="number"></span>'
+			}
+		}
+
+		vote.style.display = 'none';
+		position.innerHTML = stage.title;
+		description.innerHTML = '';
+		warn.style.display = 'none';
+		contentRight.innerHTML = '';
+		numbers.innerHTML = htmlNumber;
+	}
+
+// Functions
+function updateInterface() {
+	let stage = politicsJSON[ actualStage ];
+	let politician = stage.politics.filter( item => {
+		if ( item.number === typedNumber ) {
+			return true;
+		} else {
+			return false;
+		}
+	});
+
+	if (politician.length > 0){
+		politician = politician[0];
+		vote.style.display = 'block';
+		description.innerHTML = `Nome: ${politician.name} </br> Partido: ${politician.party}`;
+		warn.style.display = 'block';
+
+		let htmlPhotos = '';
+
+		for (let i in politician.photo) {
+			htmlPhotos += 
+				`<div class="right-image ${politician.photo[i].class}">
+					<img src="assets/images/${politician.photo[i].url}" alt="${politician.photo[i].legend}">
+					${politicsJSON[ actualStage ].title}
+				</div>`
+;		}
+
+		contentRight.innerHTML = htmlPhotos;
+	} else {
+		vote.style.display = 'block';
+		warn.style.display = 'block';
+		description.innerHTML = '<div class="big-warn blink">Voto Nulo</div>'
+	}
+
+	console.log( politician )
+}
+
+function clicked( n ) {
+	let number = qs( '.number.blink' );
+	
+	if (number !== null) {
+		number.innerHTML = n;
+		typedNumber += n;
+
+		number.classList.remove( 'blink' );
+		
+		if (number.nextElementSibling !== null) {
+			number.nextElementSibling.classList.add( 'blink' );
+		} else {
+			updateInterface();
+		}
+	}
+}
+
+/*
+function white() {
+
+}
+
+function del() {
+
+}
+
+function confirm() {
+
+}*/
+
+// Function Execution
+beginStage();
